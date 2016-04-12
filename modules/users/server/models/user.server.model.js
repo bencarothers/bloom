76c41482eec35ab5814381051c2d8e0a2fc55186
Ben Carothers <benjamin.carothers13@ncf.edu>
2016-04-12 19:15:43 -0400

    allowing @import for internal css imports

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -74,6 +74,7 @@ var UserSchema = new Schema({
     type: String,
     required: 'Provider is required'
   },
+  brews: [Schema.Types.ObjectId],
   providerData: {},
   additionalProvidersData: {},
   roles: {
@@ -177,7 +178,7 @@ UserSchema.statics.generateRandomPassphrase = function () {
     var password = '';
     var repeatingCharacters = new RegExp('(.)\\1{2,}', 'g');
 
-    // iterate until the we have a valid passphrase. 
+    // iterate until the we have a valid passphrase.
     // NOTE: Should rarely iterate more than once, but we need this to ensure no repeating characters are present.
     while (password.length < 20 || repeatingCharacters.test(password)) {
       // build the random password
